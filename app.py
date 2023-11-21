bc89ba7085440019dae7ccf4d2c71ae92ff6bfee
Syed Mohammad Ali Raza Naqvi <razanaqvi2004@gmail.com>
2023-11-21 14:06:05 +0500

    changed the input fields

--- app.py
+++ app.py
@@ -6,53 +6,58 @@ app = Flask(__name__)
 
 model = pickle.load(open("random_forest_regression_model.pkl", "rb"))
 
+years = list(range(1993, 2024))
+
 
 @app.route("/", methods=["GET"])
 def Home():
-    return render_template("index.html")
+    return render_template("index.html", years=years)
 
 
 @app.route("/predict", methods=["POST"])
 def predict():
     if request.method == "POST":
+        year = 2023 - int(request.form["year"])
+
         present_price = float(request.form["present_price"])
 
-        kms_driven = float(request.form["kms_driven"])
+        kilometers_driven = float(request.form["kilometers_driven"])
 
         owner = int(request.form["owner"])
 
-        year = int(request.form["year"])
-        year = 2023 - year
-
         fuel_type = request.form["fuel_type"]
-        fuel_type_diesel = 1 if fuel_type == "Diesel" else 0
-        fuel_type_petrol = 1 if fuel_type == "Petrol" else 0
+        fuel_type_diesel = 1 if fuel_type == "diesel" else 0
+        fuel_type_petrol = 1 if fuel_type == "petrol" else 0
 
         seller_type = request.form["seller_type"]
-        seller_type_individual = 1 if seller_type == "Individual" else 0
+        seller_type_individual = 1 if seller_type == "individual" else 0
 
         transmission_type = request.form["transmission_type"]
-        transmission_manual = 1 if transmission_type == "Manual" else 0
+        transmission_manual = 1 if transmission_type == "manual" else 0
 
-        prediction_data = np.array(
-            [
-                [
-                    present_price,
-                    kms_driven,
-                    owner,
-                    year,
-                    fuel_type_diesel,
-                    fuel_type_petrol,
-                    seller_type_individual,
-                    transmission_manual,
-                ]
-            ]
+        predicted_price = round(
+            model.predict(
+                np.array(
+                    [
+                        [
+                            present_price,
+                            kilometers_driven,
+                            owner,
+                            year,
+                            fuel_type_diesel,
+                            fuel_type_petrol,
+                            seller_type_individual,
+                            transmission_manual,
+                        ]
+                    ]
+                )
+            )[0],
+            2,
         )
-
-        predicted_price = model.predict(prediction_data)[0]
 
         return render_template(
             "index.html",
+            years=years,
             prediction_text=f"You can sell this car for {predicted_price:.2f} Lacs",
         )
 
